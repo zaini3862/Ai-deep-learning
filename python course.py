ef5a98114581a8b2c7b9c7b788b996d7cb5d6dbd
zainulabideen <zainulabideenbaggera@gmail.com>
2025-01-08 19:12:08 +0500

    instructions for git

--- python course.py
+++ python course.py
@@ -11,3 +11,16 @@ for i in range(5):
 
 # Display the final dictionary
 print("Final dictionary:", user_dict)
+
+# this is all about git
+"""git init
+git add "file name"
+git commit -m "i am changing to add comments about git inststructions
+git ask who you are email and name
+after this detail
+it commits
+now git push
+error 
+give remote
+git remote add origin https://github.com/zaini3862
+git push --set-upstrem origin master for branch selection""""
